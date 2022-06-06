90c40bcd7465a1e47e83ab7a26c27391f9c057e4
NGounakis <xgounakis5@gmail.com>
2022-06-06 12:54:57 +0300

    Add export collection as csv for ML

--- src/collection.py
+++ src/collection.py
@@ -1,6 +1,10 @@
+#%%
 import requests
 import random
 from metrics import metrics
+from models.wordEmbeddings import getWordEmbeddings
+from WekaApi import createWeka
+import pandas as pd
 
 class collcetion:
 
@@ -15,13 +19,24 @@ class collcetion:
         collection  = requests.get("http://users.ics.forth.gr/~mountant/files/project_collection.json")
         collection = collection.json()
 
-
         for topic in collection:
             for q in topic["qa"]:
-                if (q["type"] == "factoid"): 
-                    self.factoidTypes.add(q["entity"])
+                if (q["type"] == "factoid"):
+                    entity = q["entity"]
+                    # reduce multiple classes
+                    if(entity == "GPE" or entity == "ORG" or entity == "ORG, GPE"):
+                       self.factoidTypes.add("ORG-GPE")
+                       q["entity"] = "ORG-GPE"
+                    elif(entity == "TIME" or entity == "DATE" or entity == "DATE, TIME"):
+                       self.factoidTypes.add("DATE-TIME")
+                       q["entity"] = "DATE-TIME"
+                    else:
+                        self.factoidTypes.add(entity)
 
         self.topics = collection
+        print(self.answerTypes)
+        print(self.factoidTypes)
+
 
     def getRandomAnswers(self):
         '''
@@ -34,11 +49,86 @@ class collcetion:
             for q in topic["qa"]:
                 randomAnswer = random.choice(text)
                 results.append(randomAnswer)
-                gold.append(q["answer"])
+                gold.append(q["answer"].replace(" ",""))
         return results,gold
+
+    def getFeaturesAndLabels(self):
+        '''
+        returns the features and the labels for:
+        Question Type Prediction
+        Entity Type Prediction
+        '''
+        Xquestion = []
+        Yquestion = []
+        Xentity = []
+        Yentity = []
+        typeClasses = ",".join([x for x in self.answerTypes])
+        entityClasses = ",".join([x for x in self.factoidTypes])
+        for t in self.topics:
+            for qa in t['qa']:
+                qEmbeddings = getWordEmbeddings(qa['question'])
+                Xquestion.append(qEmbeddings)
+                Yquestion.append(qa['type'])
+                # only for factoid
+                if(qa['type'] == 'factoid'):
+                    Xentity.append(qEmbeddings)
+                    Yentity.append(qa['entity'])
+        return Xquestion,Yquestion,Xentity,Yentity
+
+    def exportAsWeka(self):
+        '''
+        exports the collection as two Weka files for:
+        Question Type Prediction
+        Entity Type Prediction
+        '''
+        Xquestion,Yquestion,Xentity,Yentity = self.getFeaturesAndLabels()
+        typeClasses = ",".join([x for x in self.answerTypes])
+        entityClasses = ",".join([x for x in self.factoidTypes])
+        createWeka('./Weka/qtypes',X=Xquestion,Y=Yquestion,classes=typeClasses)
+        createWeka('./Weka/etypes',X=Xentity,Y=Yentity,classes=entityClasses)
+
+    def exportDataCsv(self):
+        '''
+        exports the collection as CSV for Machine Learning:
+        Question Type Prediction
+        Entity Type Prediction
+        '''
+        Xquestion,Yquestion,Xentity,Yentity = self.getFeaturesAndLabels()
+        
+        Qdf = []
+        for x,y in zip(Xquestion,Yquestion):
+            row = list(x)
+            row.append(y)
+            Qdf.append(row)
+        Qdf = pd.DataFrame(Qdf)
+        Qdf = Qdf.set_axis([*Qdf.columns[:-1],"class"],axis=1,inplace=False)
+
+        edf = []
+        for x,y in zip(Xentity,Yentity):
+            row = list(x)
+            row.append(y)
+            edf.append(row)
+        edf = pd.DataFrame(edf)
+        edf = edf.set_axis([*edf.columns[:-1],"class"],axis=1,inplace=False)
+
+        Qdf.to_csv('./sklearn/questions.csv',index=None)
+        edf.to_csv('./sklearn/entities.csv',index=None)
+
+
+        
+
+
 
             
 
 c = collcetion()
-pred,gold = c.getRandomAnswers()
-print(metrics.exactMatch(pred,gold))
+# pred,gold = ["nikos goun","fruit juice","apple"],["nikos goun 4os","fruit juice","orange"]
+# print("EM: "+str(metrics.exactMatch(pred,gold)))
+# print("F1: "+str(metrics.f1score(pred,gold)))
+
+# print(c.answerTypes)
+# print(c.factoidTypes)
+
+c.exportDataCsv()
+
+# %%
